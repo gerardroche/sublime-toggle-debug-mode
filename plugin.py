412cde17ec65c0391906186cb36e087c7f72070d
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-20 07:23:28 +0100

    Add version constraints

--- plugin.py
+++ plugin.py
@@ -7,15 +7,23 @@ class ToggleDebugModeCommand(sublime_plugin.ApplicationCommand):
     enabled = False
 
     def description(self):
-        return '%s Debug Mode' % ('Disable' if self.enabled else 'Enable')
+        return '%s Debug Mode' % ('Disable' if self.enabled else 'Enable',)
 
     def run(self):
         self.enabled = not self.enabled
 
-        sublime.log_build_systems(self.enabled)
         sublime.log_commands(self.enabled)
-        sublime.log_indexing(self.enabled)
         sublime.log_input(self.enabled)
         sublime.log_result_regex(self.enabled)
+
+        if int(sublime.version()) >= 3009:
+            sublime.log_indexing(self.enabled)
+
+        if int(sublime.version()) >= 3076:
+            sublime.log_build_systems(self.enabled)
+
         if int(sublime.version()) >= 4064:
             sublime.log_control_tree(self.enabled)
+
+        if int(sublime.version()) >= 4075:
+            sublime.log_fps(self.enabled)
