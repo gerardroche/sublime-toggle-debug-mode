0aee33f28f96bdf505ac94487b972aeea2a71884
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-20 07:09:32 +0100

    Initial version

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ A Sublime Text plugin to toggle debug mode.
 **Pending availability in Package Control.**
 
 Install via [Package Control](https://packagecontrol.io/packages/ToggleDebugMode).
+
+## Commands
+
+Command                         | Description
+:------------------------------ | :----------
+**Toggle&nbsp;Debug&nbsp;Mode** | Toggle debug mode.
---
+++ plugin.py
@@ -0,0 +1,21 @@
+import sublime
+import sublime_plugin
+
+
+class ToggleDebugModeCommand(sublime_plugin.ApplicationCommand):
+
+    enabled = False
+
+    def description(self):
+        return '%s Debug Mode' % ('Disable' if self.enabled else 'Enable')
+
+    def run(self):
+        self.enabled = not self.enabled
+
+        sublime.log_build_systems(self.enabled)
+        sublime.log_commands(self.enabled)
+        # sublime.log_indexing(self.enabled)
+        sublime.log_input(self.enabled)
+        sublime.log_result_regex(self.enabled)
+        if int(sublime.version()) >= 4064:
+            sublime.log_control_tree(self.enabled)
