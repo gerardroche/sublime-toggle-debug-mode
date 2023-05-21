9142129aea660a112bf0ba4d92f84847ad1588e1
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-21 23:33:58 +0100

    cs

--- plugin.py
+++ plugin.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 
 
-class ToggleDebugModeCommand(sublime_plugin.ApplicationCommand):
+class ToggleDebugMode(sublime_plugin.ApplicationCommand):
 
     enabled = False
 
